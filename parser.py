3238b5b1c8b424244661117a1f985a21188f230f
William Dizon <wdchromium@gmail.com>
2025-06-20 16:08:40 -0700

    made receipt class

--- parser.py
+++ parser.py
@@ -23,24 +23,29 @@ class Attachment:
         self.filepath = None
 
 
+class Receipt:
+    def __init__(self):
+        self.delivery = False
+        self.read = False
+        self.sync = False
+        self.call = False
+        self.answer = False
+        self.hangup = False
+        self.offer = False
+        self.ice = False
+
+
 class EnvelopeParser:
     def __init__(self):
         self.sender = Contact()
         self.recipient = Contact()
         self.quote_author = Contact()
+        self.receipt = Receipt()
         self.attachment = []
         self.timing = Timestamps()
         self.body = None
         self.quote = None
         self.quoted_timestamp = None
-        self.delivery_receipt = False
-        self.read_receipt = False
-        self.sync_receipt = False
-        self.call_receipt = False
-        self.answer_receipt = False
-        self.hangup_receipt = False
-        self.offer_receipt = False
-        self.ice_receipt = False
         self.confirmed = []
 
     def __str__(self):
@@ -53,14 +58,14 @@ class EnvelopeParser:
 
         retval = f"""From: {self.sender.name} ({self.sender.number}) [dev {self.sender.device}]"""
 
-        if self.call_receipt:
-            if self.answer_receipt:
+        if self.receipt.call:
+            if self.receipt.answer:
                 retval += f"""\nTo: {self.recipient.name} ({self.recipient.number}) [dev {self.recipient.device}]"""
-            elif self.ice_receipt:
+            elif self.receipt.ice:
                 retval += f"""\nTo: {self.recipient.name} ({self.recipient.number}) [dev {self.recipient.device}]"""
-            elif self.offer_receipt:
+            elif self.receipt.offer:
                 retval += f"""\nTo: {self.recipient.name} ({self.recipient.number})"""
-            elif self.hangup_receipt:
+            elif self.receipt.hangup:
                 retval += f"""\nTo: {self.recipient.name} ({self.recipient.number})"""
             else:
                 retval += f"""\nTo: {self.recipient.name} ({self.recipient.number})"""
@@ -80,24 +85,24 @@ class EnvelopeParser:
         if self.body:
             retval += f"""\nMessage: {self.body}"""
 
-        if self.delivery_receipt:
+        if self.receipt.delivery:
             for drts in self.confirmed:
                 retval += f"\nDelivery: {drts}"
 
-        if self.read_receipt:
+        if self.receipt.read:
             for rrts in self.confirmed:
                 retval += f"\nRead: {rrts}"
 
-        if self.sync_receipt:
+        if self.receipt.sync:
             for srts in self.confirmed:
                 retval += f"\nSync: {srts}"
 
-        if self.call_receipt:
-            if self.ice_receipt:
+        if self.receipt.call:
+            if self.receipt.ice:
                 retval += f"\nCall: ONGOING"
-            elif self.offer_receipt:
+            elif self.receipt.offer:
                 retval += f"\nCall: OFFERING"
-            elif self.hangup_receipt:
+            elif self.receipt.hangup:
                 retval += f"\nCall: ENDED"
             else:
                 retval += f"\nCall: ANSWERED"
@@ -191,29 +196,29 @@ class EnvelopeParser:
             elif oneline.startswith("Destination device id:"):
                 retval.recipient.device = int(oneline.split(": ")[1])
             elif oneline == "Is delivery receipt":
-                retval.delivery_receipt = True
+                retval.receipt.delivery = True
             elif oneline == "Is read receipt":
-                retval.read_receipt = True
+                retval.receipt.read = True
             elif oneline == "Received a call message":
-                retval.call_receipt = True
+                retval.receipt.call = True
             elif oneline == "Ice update messages:":
-                retval.ice_receipt = True
+                retval.receipt.ice = True
             elif oneline.startswith("Answer message:"):
-                retval.answer_receipt = True
+                retval.receipt.answer = True
             elif oneline.startswith("Hangup message:"):
-                retval.hangup_receipt = True
+                retval.receipt.hangup = True
             elif oneline.startswith("Offer message:"):
-                retval.offer_receipt = True
+                retval.receipt.offer = True
             elif oneline == "Received sync read messages list":
-                retval.sync_receipt = True
+                retval.receipt.sync = True
                 retval.recipient.name = retval.sender.name
-            elif oneline.startswith("-") and retval.delivery_receipt:
+            elif oneline.startswith("-") and retval.receipt.delivery:
                 retval.confirmed.append(int(oneline.split(" ")[1]))
-            elif oneline.startswith("-") and retval.read_receipt:
+            elif oneline.startswith("-") and retval.receipt.read:
                 retval.confirmed.append(int(oneline.split(" ")[1]))
-            elif oneline.startswith("-") and retval.ice_receipt:
+            elif oneline.startswith("-") and retval.receipt.ice:
                 retval.confirmed.append(int(oneline.split(" ")[4]))
-            elif oneline.startswith("- From:") and retval.sync_receipt:
+            elif oneline.startswith("- From:") and retval.receipt.sync:
                 all_words = shlex.split(oneline)
 
                 sender_name = list(
